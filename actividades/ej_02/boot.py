045404b4627987ad7901053aec6b07206695d10c
LucasMeabrio <83605982+LucasMeabrio@users.noreply.github.com>
2024-08-17 21:01:34 -0300

    Update boot.py

--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,1 +1,17 @@
-# Configuracion inicial
+def conectar_wifi():
+    import network
+    from time import sleep
+    wifi = network.WLAN(network.STA_IF)
+    
+    if not wifi.isconnected():
+        print("Intentando conectarse a la red...")
+        wifi.active(True)
+        wifi.connect("Cooperadora Alumnos", "")
+        
+        while not wifi.isconnected():
+            print("...", end="")
+            sleep(0.25)
+    
+    print("Configuración de red:", wifi.ifconfig())
+
+conectar_wifi()
